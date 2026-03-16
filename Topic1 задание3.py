cce508c95e10a35651999c4ac0d4f3927892d74d
Sheraliev Sherali <sheraliev55555@gmail.com>
2026-03-16 11:55:14 +0500

    Update Topic1 задание3.py

--- Topic1 задание3.py
+++ Topic1 задание3.py
@@ -1,11 +1,8 @@
 # Запрашиваем номер карты (16 цифр)
 card_number = input("Введите номер карты (16 цифр): ")
 
-# Извлекаем последние 4 цифры
-last_four = card_number[12:16]  # или card_number[-4:]
-
 # Формируем замаскированный номер
-masked_number = f"**** **** **** {last_four}"
+masked_number = "**** **** **** " + card_number[-4:]
 
 # Выводим результат
 print(masked_number)
